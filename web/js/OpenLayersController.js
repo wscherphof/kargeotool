30ce798efd6f6ee20615dd4811d7e5c3ef86d20c
Meine Toonen <toonen.meine@gmail.com>
2013-01-02 11:30:20 +0000

    Added measuring functionality and some comments

--- web/js/OpenLayersController.js
+++ web/js/OpenLayersController.js
@@ -1,6 +1,7 @@
 function ol (){
     this.map = null;
     this.panel = null;
+    // Make the map
     this.createMap = function(domId){
         this.panel = new OpenLayers.Control.Panel();
         var maxBounds = new OpenLayers.Bounds(12000,304000,280000,620000);
@@ -11,11 +12,18 @@ function ol (){
             srs: 'epsg:28992', 
             allOverlays: true,
             theme: OpenLayers._getScriptLocation()+'theme/b3p/style.css',
-            units : 'meters',
+            units : 'm',
             controls : [new OpenLayers.Control.PanZoomBar(), new OpenLayers.Control.Navigation(), this.panel]
         };
         
         this.map = new OpenLayers.Map(domId,opt);
+        this.createControls();
+       
+    },
+    /**
+     * Private nethod which adds all the controls
+     */
+    this.createControls = function (){
         this.panel.addControls(new OpenLayers.Control.DragPan()); 
         this.panel.addControls(new OpenLayers.Control.ZoomBox()); 
         this.panel.addControls( new OpenLayers.Control.ZoomToMaxExtent()); 
@@ -24,8 +32,58 @@ function ol (){
         this.panel.addControls( navHist.previous);
         this.panel.addControls( navHist.next);
         this.map.addControl( new OpenLayers.Control.MousePosition({numDigits: 2}));
-       
+        
+        var options = new Object();
+        options["persist"]=true;
+        options["callbacks"]={
+            modify: function (evt){
+                //make a tooltip with the measured length
+                if (evt.parent){
+                    var measureValueDiv=document.getElementById("olControlMeasureValue");
+                    if (measureValueDiv==undefined){
+                        measureValueDiv=document.createElement('div');
+                        measureValueDiv.id="olControlMeasureValue";
+                        measureValueDiv.style.position='absolute';
+                        this.map.div.appendChild(measureValueDiv);
+                        measureValueDiv.style.zIndex="10000";
+                        measureValueDiv.className="olControlMaptip";
+                        var measureValueText=document.createElement('div');
+                        measureValueText.id='olControlMeasureValueText';
+                        measureValueDiv.appendChild(measureValueText);
+                    }
+                    var px= this.map.getViewPortPxFromLonLat(new OpenLayers.LonLat(evt.x,evt.y));
+                    measureValueDiv.style.top=px.y+"px";
+                    measureValueDiv.style.left=px.x+25+'px'
+                    measureValueDiv.style.display="block";
+                    var measureValueText=document.getElementById('olControlMeasureValueText');
+                    var bestLengthTokens=this.getBestLength(evt.parent);
+                    measureValueText.innerHTML= bestLengthTokens[0].toFixed(3)+" "+bestLengthTokens[1];
+                }
+            }
+        }
+
+        var measureTool= new OpenLayers.Control.Measure( OpenLayers.Handler.Path, options);
+        measureTool.events.register('measure',measureTool,function(){
+            var measureValueDiv=document.getElementById("olControlMeasureValue");
+            if (measureValueDiv){                
+                measureValueDiv.style.display="none";
+            }
+            this.cancel();
+        });
+        measureTool.events.register('deactivate',measureTool,function(){
+            var measureValueDiv=document.getElementById("olControlMeasureValue");
+            if (measureValueDiv){
+                measureValueDiv.style.display="none";
+            }
+        });
+        this.panel.addControls (measureTool);
     },
+    /**
+     * Add a layer. Assumed is that everything is in epsg:28992, units in meters and the maxextent is The Netherlands
+     * @param name The name of the layer
+     * @param url The url to the service
+     * @param layers The layers of the service which must be retrieved
+     */
     this.addLayer = function (name, url, layers){
          var wms = new OpenLayers.Layer.WMS(
             "OpenLayers WMS",
