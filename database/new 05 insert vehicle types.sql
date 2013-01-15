33996de72e12c9be1fc57cd5a250d80021a9b2f4
Matthijs Laan <matthijsln@gmail.com>
2013-01-15 20:14:16 +0000

    geen speciale tekens, fix haakje

--- database/new 05 insert vehicle types.sql
+++ database/new 05 insert vehicle types.sql
@@ -2,7 +2,7 @@
 -- Afkomstig uit "TMI8 KAR meldpunten (kv 9), v8.1.0.0, release.pdf", pagina 17
 
 insert into vehicle_type(nummer,omschrijving) values
-  (0,'Gereserveerd – niet gebruiken'),
+  (0,'Gereserveerd - niet gebruiken'),
   (1,'Bus'),
   (2,'Tram'),
   (3,'Politie'),
@@ -13,12 +13,12 @@ insert into vehicle_type(nummer,omschrijving) values
   (69,'Politie niet in uniform'),
   (70,'Marechaussee'),
   (71,'Hoogwaardig Openbaar Vervoer (HOB) bus'),
-  (99,'Gereserveerd – niet gebruiken');
+  (99,'Gereserveerd - niet gebruiken');
 
 insert into vehicle_type(nummer,omschrijving)
-  select generate_series(8,68), 'Gereserveerd – niet gebruiken');
+  select generate_series(8,68), 'Gereserveerd - niet gebruiken';
 
 insert into vehicle_type(nummer,omschrijving)
-  select generate_series(72,98), 'Gereserveerd – vrij te gebruiken');
+  select generate_series(72,98), 'Gereserveerd - vrij te gebruiken';
 
 
