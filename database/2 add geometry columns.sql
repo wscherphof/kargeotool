35672895619e9d48b49b37befca5141855badffa
Matthijs Laan <matthijsln@gmail.com>
2013-01-24 08:54:15 +0000

    meer oude dingen verwijderd

--- database/2 add geometry columns.sql
+++
@@ -1,3 +0,0 @@
-select addGeometryColumn('public','activation','location',28992,'POINT',2);
-select addGeometryColumn('public','roadside_equipment','location',28992,'POINT',2);
-select addGeometryColumn('public','activation_group','stop_line_location',28992,'POINT',2);
