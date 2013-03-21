ffa59be67c38bb7b9bee46fdd8165a832bc6165f
Meine Toonen <toonen.meine@gmail.com>
2013-03-21 14:03:24 +0000

    Contextmenu works also with Overview! It's amazing!

--- web/js/overview.js
+++ web/js/overview.js
@@ -118,6 +118,9 @@ Ext.define("nl.b3p.kar.Overview",{
                             }
                         }
                     }
+                },
+                itemcontextmenu : function (view,record,item,index,event,eOpts){
+                    this.editor.contextMenu.show(event.xy[0], event.xy[1],false);
                 }
             }
         });
