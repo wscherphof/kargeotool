498025ac04e637453a6fd086029859c6919208ec
Meine Toonen <toonen.meine@gmail.com>
2013-05-01 15:13:31 +0000

    using sldActionBeanUrl instead of sldUrl

--- web/js/search.js
+++ web/js/search.js
@@ -460,7 +460,7 @@ Ext.define("nl.b3p.kar.SearchBusline", {
             var link = Ext.get(addresslink);
             var me = this;
             link.on('click', function() {
-                var sld = sldUrl + '?publicnumber=';
+                var sld = sldActionBeanUrl + '?publicnumber=';
                 sld+= busline.publicnumber;
                 editor.olc.addSldToKargis(sld);
                 var env = busline.envelope;
