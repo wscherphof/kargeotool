0950be2404b8435982f76145c3095bc443e723f7
Meine Toonen <toonen.meine@gmail.com>
2013-05-15 14:12:42 +0000

    correct tables for inserting

--- database/03 insert data owners.sql
+++ database/03 insert data owners.sql
@@ -1,7 +1,7 @@
 
 -- Afkomstig uit "TMI8 KAR meldpunten (kv 9), v8.1.0.0, release.pdf", pagina 16
 
-insert into data_owner2(code, classificatie, company_number, omschrijving) values
+insert into data_owner(code, classificatie, company_number, omschrijving) values
  ('ARR', 'Vervoerder', 1, 'Arriva Personenvervoer Nederland'),
  ('VTN', 'Vervoerder', 2, 'Veolia Transport Nederland'),
  ('CXX', 'Vervoerder', 3, 'Connexxion, Hermes (incl GVU, Novio, Stadsvervoer NL)'),
@@ -19,16 +19,16 @@ insert into data_owner2(code, classificatie, company_number, omschrijving) value
  ('SABIMOS', 'Integrator', null, 'Satellite Based Information and Management Operating System'),
  ('PRORAIL', 'Integrator', null, 'ProRail');
  
-insert into data_owner2(code, classificatie, company_number, omschrijving) values
+insert into data_owner(code, classificatie, company_number, omschrijving) values
  ('B3P', 'Integrator', null, 'B3Partners');
 
-insert into data_owner2(code, classificatie, company_number, omschrijving) select
+insert into data_owner(code, classificatie, company_number, omschrijving) select
     'CBSPV' || unnest(ARRAY['0001','0002','0003','0004','0005','0006','0007','0008','0009','0010','0011','0012']),
     'Infra Beheerder',
     null,
     unnest(ARRAY['Provincie Groningen','Provincie Fryslân','Provincie Drenthe','Provincie Overijssel','Provincie Gelderland','Provincie Utrecht','Provincie Noord-Holland','Provincie Zuid-Holland','Provincie Zeeland','Provincie Noord-Brabant','Provincie Limburg','Provincie Flevoland']);
 
-insert into data_owner2(code, classificatie, company_number, omschrijving) select
+insert into data_owner(code, classificatie, company_number, omschrijving) select
     'RWS' || unnest(ARRAY['BD','DVS','DID','WD','DNH','DZH','DUT','DIJG','DNN','DON','DZL','DNB','DLB','DNZ']),
     'Infra Beheerder',
     null,
@@ -37,7 +37,7 @@ insert into data_owner2(code, classificatie, company_number, omschrijving) selec
     'Regionale Dienst IJsselmeer Gebied','Regionale Dienst Noord-Nederland','Regionale Dienst Oost-Nederland',
     'Regionale Dienst Zeeland','Regionale Dienst Noord-Brabant','Regionale Dienst Limburg','Regionale Dienst Noordzee']);
     
-insert into data_owner2(code, classificatie, company_number, omschrijving) select
+insert into data_owner(code, classificatie, company_number, omschrijving) select
     'INFRA' || unnest(ARRAY['AAS','HBR']),
     'Infra Beheerder',
     null,
