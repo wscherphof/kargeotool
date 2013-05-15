9b72b231406e66fcf25d4a996b8eb700303d7c61
Meine Toonen <toonen.meine@gmail.com>
2013-05-15 14:11:57 +0000

    correct columns for dropping and adding geometry columns

--- database/01 add geometry columns.sql
+++ database/01 add geometry columns.sql
@@ -1,4 +1,4 @@
-alter table activation_point2 drop column location;
-alter table roadside_equipment2 drop column location;
-select addGeometryColumn('data','activation_point2','location',28992,'POINT',2);
-select addGeometryColumn('data','roadside_equipment2','location',28992,'POINT',2);
+alter table activation_point drop column location;
+alter table roadside_equipment drop column location;
+select addGeometryColumn('data','activation_point','location',28992,'POINT',2);
+select addGeometryColumn('data','roadside_equipment','location',28992,'POINT',2);
