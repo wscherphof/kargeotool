bccbb12bbaac5b1d96222b6601ba323dd59e910a
Meine Toonen <toonen.meine@gmail.com>
2013-11-07 12:50:57 +0000

    Bugfix: vehicle types for existing inmeldpunten come from the uitmeldpunt to which the inmeldpunt must be added

--- web/js/models.js
+++ web/js/models.js
@@ -323,10 +323,10 @@ Ext.define('RSEQ', {
                 pointId: inmeldpunt.getId()
             });     
             // De voor dit inmeldpunt ingestelde inmeldpuntwaardes
-            Ext.Object.merge(newMap, objectSubset(theMap, ["distanceTillStopLine","vehicleTypes","virtualLocalLoopNumber", "triggerType"]));
+            Ext.Object.merge(newMap, objectSubset(theMap, ["distanceTillStopLine","virtualLocalLoopNumber", "triggerType"]));
             
             // De signal waardes komen van de MovementActivationPoint van het uitmeldpunt
-            Ext.Object.merge(newMap, objectSubset(mvmtAndMap.map, ["signalGroupNumber"]));
+            Ext.Object.merge(newMap, objectSubset(mvmtAndMap.map, ["signalGroupNumber","vehicleTypes"]));
 
             mvmtAndMap.movement.maps.splice(0, 0, newMap);
         });
